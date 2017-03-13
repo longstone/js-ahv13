6b1c2e34c63544dc7fd03c70a943eb235a23f81e
longstone <git@longstone.ch>
2017-03-13 22:38:37 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+class ValidatorAHV13 {
+
+    constructor() {
+    }
+
+    _removeDots(ahv13){
+        return ahv13.split('.').join("");
+    }
+
+    /**
+     * AHV Number in reversed order, without checksum
+     * @param ahv12
+     * @returns {number}
+     * @private
+     */
+    calculateCheckSum(ahv12){
+        let totalChecksum=0;
+        for(let i = 0; i<ahv12.length;i++){
+            const number = ahv12[i];
+            if(i%2===0){
+                totalChecksum = totalChecksum + 13 * number;
+            }else{
+                totalChecksum = totalChecksum + number;
+            }
+        }
+        return totalChecksum % 10;
+    }
+
+    /**
+     * validates a AHV13 Number with or without dots in the form of '756.9217.0769.85'
+     * @param ahv13
+     * @returns {boolean}
+     */
+    isValid(ahv13) {
+        const cleanedAHV13 = this._removeDots(ahv13);
+        const ahvArray = cleanedAHV13.split('').reverse().map( number => parseInt(number));
+        const check = ahvArray.shift();
+        return this.calculateCheckSum(ahvArray)  === check;
+    }
+}
+module.exports = ValidatorAHV13;
---
+++ src/index.test.js
@@ -0,0 +1,39 @@
+/**
+ * Created by longstone on 13.03.17.
+ */
+const assert = require("assert");
+const AHV13 = require('./index');
+
+
+describe('check ahv13 validity',  ()  => {
+
+    it('should return that the sample case is valid',  () =>  {
+        const ahv13validator = new AHV13();
+       const ahv13number = '756.9217.0769.85';
+        const expected = true;
+        const actual =ahv13validator.isValid(ahv13number);
+        assert.equal(actual, expected);
+    });
+
+    it('should return that the sample case is valid with or without dots',  () =>  {
+        const ahv13validator = new AHV13();
+        const ahv13numberDotted = '756.9217.0769.85';
+        const ahv13number = '7569217076985';
+        const expected = true;
+        const actual =ahv13validator.isValid(ahv13number);
+        const actualDotted =  ahv13validator.isValid(ahv13numberDotted);
+        assert.equal(actual, expected);
+        assert.equal(actualDotted, expected);
+    });
+    it('should fail if the sample  is invalid',  () =>  {
+        const ahv13validator = new AHV13();
+       const ahv13number = '756.9217.0769.83';
+        const expected = false;
+        const actual =ahv13validator.isValid(ahv13number);
+        assert.equal(actual, expected);
+    });
+
+
+
+
+});
